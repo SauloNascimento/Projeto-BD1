7b033b761f4054703b423d2e91a5bbe8cc388fc1
Tainah Emmanuele <tainahemmanuele@gmail.com>
2017-07-21 18:16:31 -0300

    Update remover_tabelas.sql
    Parte de  1 a 10 feita (Saulo, João e Tainah)

--- remover_tabelas.sql
+++ remover_tabelas.sql
@@ -1,1 +1,25 @@
+DROP TABLE DEPENDENTE;
 
+DROP TABLE Nota_Fiscal;
+
+DROP TABLE Cliente;
+
+DROP TABLE Quarto;
+
+DROP TABLE Produto;
+
+DROP TABLE Servico;
+
+DROP TABLE Funcionario;
+
+DROP SEQUENCE id_Quarto_Seq;
+
+DROP SEQUENCE id_NF_Seq;
+
+DROP SEQUENCE id_Prod_Seq;
+
+DROP TABLE Lavanderia;
+
+DROP TABLE Passeio_Turistico;
+
+DROP TABLE Frigobar;
