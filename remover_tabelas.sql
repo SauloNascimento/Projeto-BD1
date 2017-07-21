dcaea72eb414078555d270551920312b28913d9b
Tainah Emmanuele <tainahemmanuele@gmail.com>
2017-07-21 18:31:05 -0300

    Update remover_tabelas.sql

--- remover_tabelas.sql
+++ remover_tabelas.sql
@@ -8,6 +8,12 @@ DROP TABLE Quarto;
 
 DROP TABLE Produto;
 
+DROP TABLE Lavanderia;
+
+DROP TABLE Passeio_Turistico;
+
+DROP TABLE Frigobar;
+
 DROP TABLE Servico;
 
 DROP TABLE Funcionario;
@@ -17,9 +23,3 @@ DROP SEQUENCE id_Quarto_Seq;
 DROP SEQUENCE id_NF_Seq;
 
 DROP SEQUENCE id_Prod_Seq;
-
-DROP TABLE Lavanderia;
-
-DROP TABLE Passeio_Turistico;
-
-DROP TABLE Frigobar;
