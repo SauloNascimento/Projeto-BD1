e16ec60f33f640e24195fade1b02abc78edfe0a1
Tainah Emmanuele <tainahemmanuele@gmail.com>
2017-07-24 20:56:24 -0300

    Update remover_tabelas.sql
    Atualização do script

--- remover_tabelas.sql
+++ remover_tabelas.sql
@@ -2,17 +2,29 @@ DROP TABLE DEPENDENTE;
 
 DROP TABLE Nota_Fiscal;
 
+DROP TABLE Telefone_Cliente;
+
 DROP TABLE Cliente;
+
+DROP TABLE Venda;
 
 DROP TABLE Quarto;
 
 DROP TABLE Produto;
+
+DROP TABLE Prestacao;
 
 DROP TABLE Lavanderia;
 
 DROP TABLE Passeio_Turistico;
 
 DROP TABLE Frigobar;
+
+DROP TABLE Restaurante;
+
+DROP TABLE Estacionamento;
+
+DROP TABLE Bar;
 
 DROP TABLE Servico;
 
@@ -23,3 +35,4 @@ DROP SEQUENCE id_Quarto_Seq;
 DROP SEQUENCE id_NF_Seq;
 
 DROP SEQUENCE id_Prod_Seq;
+
